860aff938747151c241b17dcd3471038ffc9a3c1
Evan Jacques <evan@evans-mac-mini.local>
2016-03-23 22:19:25 -0700

    Initial Commit

---
+++ Trckr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trckr
+//
+//  Created by Evan Jacques on 2016-03-23.
+//  Copyright © 2016 Evan Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
